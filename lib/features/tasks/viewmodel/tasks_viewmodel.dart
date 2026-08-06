5ec3a0266412b9c87618bfb7ee8017595bc3260c
rdv296 <311404586+iamrdv296@users.noreply.github.com>
2026-08-06 20:22:05 +0100

    refactor(tasks): decoupled uuid from taskviewmodel

--- lib/features/tasks/viewmodel/tasks_viewmodel.dart
+++ lib/features/tasks/viewmodel/tasks_viewmodel.dart
@@ -1,22 +1,20 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:the_todo_app/core/providers/uuid_provider.dart';
 import 'package:the_todo_app/features/tasks/model/task.dart';
 import 'package:uuid/uuid.dart';
 
 part 'tasks_viewmodel.g.dart';
 
-// TODO: decouple uuid from TasksViewModel
-
 @riverpod
 class TasksViewModel extends _$TasksViewModel {
-  final _uuid = const Uuid();
-
   @override
   List<Task> build() {
     return [];
   }
 
   void addTask(String title) {
-    state = [...state, Task(title, id: _uuid.v4())];
+    final Uuid uuid = ref.read(uuidProvider);
+    state = [...state, Task(title, id: uuid.v4())];
   }
 
   void removeTask(String id) {
