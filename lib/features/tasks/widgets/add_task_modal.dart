efcda129b6a2aa99846603c5d9e7653b948e3378
rdv296 <311404586+iamrdv296@users.noreply.github.com>
2026-08-07 11:21:13 +0100

    refactor(tasks): change AddTaskModel to StatefulWidget

--- lib/features/tasks/widgets/add_task_modal.dart
+++ lib/features/tasks/widgets/add_task_modal.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 
-class AddTaskModal extends StatelessWidget {
-  const AddTaskModal({super.key, required this.onAddTask});
+class AddTaskModal extends StatefulWidget {
+  const AddTaskModal({
+    super.key,
+    required this.onAddTask,
+  });
 
   final void Function(String) onAddTask;
 
   @override
+  State<AddTaskModal> createState() => _AddTaskModalState();
+}
+
+class _AddTaskModalState extends State<AddTaskModal> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _submit() {
+    final title = _controller.text.trim();
+
+    if (title.isEmpty) return;
+
+    widget.onAddTask(title);
+    Navigator.pop(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController();
-    
     return AlertDialog(
       title: const Text('New Task'),
       content: TextField(
-        controller: controller,
+        controller: _controller,
         autofocus: true,
-        onSubmitted: (_) => _submit(controller, context),
+        onSubmitted: (_) => _submit(),
       ),
       actions: [
         TextButton(
@@ -22,19 +51,10 @@ class AddTaskModal extends StatelessWidget {
           child: const Text('Cancel'),
         ),
         ElevatedButton(
-          onPressed: () => _submit(controller, context),
+          onPressed: _submit,
           child: const Text('Add'),
         ),
       ],
     );
-  }
-
-  void _submit(TextEditingController controller, BuildContext context) {
-    final taskTitle = controller.text.trim();
-  
-    if (taskTitle.isEmpty) return;
-  
-    onAddTask(taskTitle);
-    Navigator.pop(context);
   }
 }
